9e0eba19644f8cc049bf395fc34491062c0a3d4c
murderous <my.senpaii@yandex.ru>
2024-11-15 18:05:16 +0300

    first project

---
+++ LabOne/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Oh, hi, Mark!");
+            Console.ReadKey();
+        }
+    }
+}
